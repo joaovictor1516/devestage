91241cda1f2d8b6d4bda27d746c11b722d012ab8
joaovictor1516 <voliveira.joao30@sempreceub.com>
2025-02-19 13:35:50 -0300

    desenvolvimento do componente input. ele foi desmostado em tres componentes, pois a sua estrutura estava comecando a ficar confusa e foi visto que separando as suas partes em componentes torna mais eficiente o seu desenvolvimento, os componentes estao presentes no mesmo arquivo visto que sempre serao usados juntos e os componentes InputRoot e InputIcons utilizam a classe group do tailwind para sincronizarem os seu estilos.

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,4 +1,39 @@
+"use client"
 
-export function Input(){
-	
+import { ComponentProps } from "react";
+
+interface InputRootProps extends ComponentProps<"div">{
+    error?: boolean;
+}
+
+export function InputRoot({ error = false, ...props }: InputRootProps){
+    return (
+        <div 
+            data-error={ error }
+            className="group bg-gray-800 border border-gray-600 rounded-xl flex items-center gap-2 px-4 h-12 focus-within:border-gray-100 data-[error=true]:border-danger"
+            { ...props }
+        />
+    );
+}
+
+interface InputIconsProps extends ComponentProps<"span">{}
+
+export function InputIcons(props: InputIconsProps){
+    return (
+        <span  
+            className="text-gray-400 group-focus-within:text-gray-100 group-[&:not(:has(input:placeholder-shown))]:text-gray-100 group-data-[error=true]:text-danger"
+            {...props}
+        />
+    )
+}
+
+interface InputFieldsProps extends ComponentProps<"input">{}
+
+export function InputFields(props: InputFieldsProps){
+    return (
+        <input 
+            className="flex-1 outline-0"
+            {...props}
+        />
+    )
 }
