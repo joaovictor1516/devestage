0860264be55a5f17533b095a0265a300aef7a933
joaovictor1516 <voliveira.joao30@sempreceub.com>
2025-02-19 11:11:21 -0300

    adicao do componentprops para adicionar as funcionalidades da tag button, alem da remocao do children pois o button ja tem a funcionalidade de receber elemenetos do componente pai.

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,15 +1,14 @@
 "use client"
 
-import { ReactNode } from "react"
+import { ComponentProps } from "react"
 
-interface ButtonProps{
-	children: ReactNode
-}
+interface ButtonProps extends ComponentProps<"button">{} //Como a tag button recebe valores dentro dela não é necessario a utilização do ReactNode.
 
 export function Button(props: ButtonProps){
 	return (
-		<button className="flex justify-between items-center bg-gray-500 border-blue text-blue w-full h-12 px-5 font-semibold rounded-xl cursor-pointer transition duration-300 hover:bg-blue hover:text-gray-900">
-			{props.children}
-		</button>
+		<button 
+			className="flex justify-between items-center bg-gray-500 border-blue text-blue w-full h-12 px-5 font-semibold rounded-xl cursor-pointer transition duration-300 hover:bg-blue hover:text-gray-900" 
+			{...props}
+		/> //A função do react ComponentProps nos permite utilizar as funcionalidades de uma tag do HTML.
 	)
 }
