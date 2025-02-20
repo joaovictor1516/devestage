996e86847c949b1f7ab13e109ae9ed5cf47a32da
joaovictor1516 <voliveira.joao30@sempreceub.com>
2025-02-20 14:32:54 -0300

    instalacao e utilizacao do tailwind-merge no componente iconButton, para que possamos adicionar propriedades no className do componente sem sobre-escrever os existentes, neste caso foi usado apenas a funcao twMerge.

--- src/components/iconButton.tsx
+++ src/components/iconButton.tsx
@@ -1,13 +1,17 @@
 "use client";
 
 import type { ComponentProps } from "react";
+import { twMerge } from "tailwind-merge";
 
 interface IconButtonProps extends ComponentProps<"button"> {}
 
-export function IconButton(props: IconButtonProps) {
+export function IconButton({ className, ...props }: IconButtonProps) {
 	return (
 		<button
-			className="bg-gray-500 text-blue hover:bg-blue hover:text-gray-900 p-1.5 rounded-md cursor-pointer transition duration-300"
+			className={twMerge(
+				"bg-gray-500 text-blue hover:bg-blue hover:text-gray-900 p-1.5 rounded-md cursor-pointer transition duration-300",
+				className,
+			)}
 			{...props}
 		/>
 	);
