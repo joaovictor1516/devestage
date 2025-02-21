009ed93d95aebcff7823bc705b4a98bb61129f49
joaovictor1516 <voliveira.joao30@sempreceub.com>
2025-02-21 00:26:36 -0300

    desenvolvimento da interacao do usuario e o tratamento de erro do componente responsavel pela inscricao do usuario. isso foi feito usando o useForm com o zod.

--- src/app/(home)/subscriptionForm.tsx
+++ src/app/(home)/subscriptionForm.tsx
@@ -1,34 +1,81 @@
+"use client";
+
 import { Button } from "@/components/button";
-import { InputRoot, InputIcon, InputField } from "@/components/input";
-import { User, Mail, ArrowRight } from "lucide-react";
+import { InputField, InputIcon, InputRoot } from "@/components/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { ArrowRight, Mail, User } from "lucide-react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
-export function SubscriptionForm(){
-    return (
-        <form className="bg-gray-700 border border-gray-600 rounded-2xl p-8 space-y-6 w-full md:max-w-[440px]">
-            <h2 className="font-heading font-semibold text-gray-200 text-xl">
-                inscrição
-            </h2>
+const SubscriptionFormSchema = z.object({
+	name: z.string().min(3, "Digite seu nome completo."),
+	email: z.string().email("Digite um e-mail válido"),
+});
 
-            <div className="space-y-3">
-                <InputRoot>
-                    <InputIcon>
-                        <User />
-                    </InputIcon>
-                    <InputField type="text" placeholder="Nome Completo" />
-                </InputRoot>
+type SubscriptionFormType = z.infer<typeof SubscriptionFormSchema>;
 
-                <InputRoot>
-                    <InputIcon>
-                        <Mail />
-                    </InputIcon>
-                    <InputField type="email" placeholder="E-mail" />
-                </InputRoot>
-            </div>
+export function SubscriptionForm() {
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<SubscriptionFormType>({
+		resolver: zodResolver(SubscriptionFormSchema),
+	});
 
-            <Button type="submit">
-                Confirmar
-                <ArrowRight />
-            </Button>
-        </form>
-    );
+	function onSubscribe(data: SubscriptionFormType) {
+		console.log(data);
+	}
+
+	return (
+		<form
+			onSubmit={handleSubmit(onSubscribe)}
+			className="bg-gray-700 border border-gray-600 rounded-2xl p-8 space-y-6 w-full md:max-w-[440px]"
+		>
+			<h2 className="font-heading font-semibold text-gray-200 text-xl">
+				inscrição
+			</h2>
+
+			<div className="space-y-3">
+				<div className="space-y-2">
+					<InputRoot>
+						<InputIcon>
+							<User />
+						</InputIcon>
+						<InputField
+							type="text"
+							placeholder="Nome Completo"
+							{...register("name")}
+						/>
+					</InputRoot>
+
+					{errors.name && (
+						<p className="text-danger text-xs font-semibold">
+							{errors.name.message}
+						</p>
+					)}
+				</div>
+
+				<div className="space-y-2">
+					<InputRoot>
+						<InputIcon>
+							<Mail />
+						</InputIcon>
+						<InputField placeholder="E-mail" {...register("email")} />
+					</InputRoot>
+
+					{errors.email && (
+						<p className="text-danger text-xs font-semibold">
+							{errors.email.message}
+						</p>
+					)}
+				</div>
+			</div>
+
+			<Button type="submit">
+				Confirmar
+				<ArrowRight />
+			</Button>
+		</form>
+	);
 }
