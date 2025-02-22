c1c98b83c24901444b03c88239a575ea2b8a4ed7
joaovictor1516 <voliveira.joao30@sempreceub.com>
2025-02-22 01:52:37 -0300

    melhoria da estetica do codigo da rota de inscricao do usuario.

--- apps/backend/src/routs/subscribeToEventRoute.ts
+++ apps/backend/src/routs/subscribeToEventRoute.ts
@@ -3,29 +3,29 @@ import z from "zod";
 
 export const subscribeToEventRoute: FastifyPluginAsyncZod = async (app) => {
 	app.post(
-	"/subscription",
-	{
-		schema: {
-			tags: ["subscribe"],
-			body: z.object({
-				name: z.string().min(3, "Digite seu nome completo."),
-				email: z.string().email("Digite um e-mail válido."),
-			}),
-			response: {
-				201: z.object({
-					name: z.string(),
-					email: z.string(),
+		"/subscription",
+		{
+			schema: {
+				tags: ["subscribe"],
+				body: z.object({
+					name: z.string().min(3, "Digite seu nome completo."),
+					email: z.string().email("Digite um e-mail válido."),
 				}),
+				response: {
+					201: z.object({
+						name: z.string(),
+						email: z.string(),
+					}),
+				},
 			},
 		},
-	},
-	async (request, reply) => {
-		const { name, email } = request.body;
+		async (request, reply) => {
+			const { name, email } = request.body;
 
-		return reply.status(201).send({
-			name,
-			email,
-		});
-	},
-);
-}
+			return reply.status(201).send({
+				name,
+				email,
+			});
+		},
+	);
+};
