b70eb050994b555bcf4bf60432d21d48c99bdff2
joaovictor1516 <voliveira.joao30@sempreceub.com>
2025-02-22 16:50:43 -0300

    criacao e adicao da tabela de inscricoes de usuarios.

--- apps/backend/src/drizzle/client.ts
+++ apps/backend/src/drizzle/client.ts
@@ -1,6 +1,11 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import { env } from "../env";
+import { subscriptions } from "./schema/subscriptions";
 
 export const postgresSql = postgres(env.POSTGRES_URL);
-export const database = drizzle(postgresSql);
+export const database = drizzle(postgresSql, {
+	schema: {
+		subscriptions,
+	},
+});
